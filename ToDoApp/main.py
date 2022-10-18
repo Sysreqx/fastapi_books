90a1ba281d61964a315ed946429addcc353305ee
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-10-18 17:42:11 +0600

    feature(all): Get All Todos from Database

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
-
-
-app = FastAPI()
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
-async def create_database():
-    return {"Database": "Created"}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
+
