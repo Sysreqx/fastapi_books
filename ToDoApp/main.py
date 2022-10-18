127af55b3fa80b6c28cef091d7ce7837c7f51f84
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-10-18 18:08:35 +0600

    feature(all): Get Todo by ID

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -20,3 +20,17 @@ def get_db():
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
 
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+
+    raise http_exception()
+
+
+def http_exception():
+    return HTTPException(status_code=404, detail="Todo not found")
